afac6e15fe55047a48a874c199180d84a2438a3c
Pedro Rocha <pedrovqrocha@gmail.com>
2021-10-27 08:08:55 -0300

    feat(websocket): add websocket serve and simple client

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.json());
 
 require('./controllers/messagesController')(app);
+
+io.on('connection', socket => {
+  console.log(`${socket.id} connected`);
+
+  socket.on('sendMessage', data => {
+    console.log(data);
+  });
+});
 
 app.listen(process.env.PORT);
