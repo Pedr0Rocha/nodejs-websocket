08f25f2aa12f2a52f2fd41016370a898774c8c27
Pedro Rocha <pedrovqrocha@gmail.com>
2021-10-27 23:17:56 -0300

    feat(websocket): add save message feature and client

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 app.use(express.json());
+app.use(cors());
 
 require('./controllers/messagesController')(app);
+
+const Message = require('./models/message');
 
 io.on('connection', socket => {
   console.log(`${socket.id} connected`);
 
   socket.on('sendMessage', data => {
     console.log(data);
+
+    if (!data.key || !data.value) {
+      return;
+    }
+
+    Message.create(data);
   });
 });
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
